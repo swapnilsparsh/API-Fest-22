97660276a025f22063d1078ed607b8fc31eb94e8
Bhavya <bhavya.sachdeva.58@gmail.com>
2022-01-28 16:50:07 +0530

    Investment-in-progress

--- server/poetry.js
+++ server/poetry.js
@@ -8,7 +8,7 @@ module.exports = [{
 },
 {
 "id":"1",
-"img": "",
+"img": "https://www.sffchronicles.com/images/logo14.png",
 "title": "Chronicles",
 "info": "The Chronicles is another writing forum, this time focusing specifically on science fiction and fantasy genres.",
 "link": "https://www.sffchronicles.com/",
@@ -16,15 +16,15 @@ module.exports = [{
 },
 {
 "id":"2",  
-"img": "",
+"img": "https://scontent.fdel38-1.fna.fbcdn.net/v/t1.6435-9/50820506_2066801483395595_6687056370958598144_n.png?_nc_cat=104&ccb=1-5&_nc_sid=973b4a&_nc_ohc=vnWF3zKs2SEAX-V3IWk&_nc_ht=scontent.fdel38-1.fna&oh=00_AT99UuQ8ZRXKS-UtBFOimR-mDRQOzZn4YKjwgeN_FI-X1w&oe=6218F3FA",
 "title": "Delhi Poetry Slam",
 "info": "A club that not only provides you with a platform to pursue your passion, but also to develop it more professionally, what’s not to love about Delhi Poetry Slam!",
-"link": "https://www.facebook.com/delhipoetryslam",
+"link": "https://www.delhipoetryslam.com/",
 "rating": "3"
 },
 {
 "id":"3",    
-"img": "",
+"img": "https://scontent.fdel38-1.fna.fbcdn.net/v/t1.6435-9/54369282_2578891232153320_734552184867258368_n.jpg?_nc_cat=103&ccb=1-5&_nc_sid=09cbfe&_nc_ohc=NV6bH2D-EIsAX8lfc0Q&_nc_oc=AQkhyI8MiIG_8RuBYY20lS-15X0qrFckx403urWEWBavZXaM802fpeBP4YxM4c4zZTw&_nc_ht=scontent.fdel38-1.fna&oh=00_AT-T4oyj0J077hVtXTxnJBhWnMjBwsriaPrNgFGJTYAJFQ&oe=6217C791",
 "title": "Poetry Couture",
 "info": "Poetry Couture is a not-for-profit literary initiative with the objective of reviving the poetry reading culture and foster poetry as a performing art for the youth and poetry lovers alike.",
 "link": "https://www.facebook.com/poetcouture/",
@@ -32,15 +32,15 @@ module.exports = [{
 },
 {
  "id":"4",   
-"img": "",
+"img": "https://scontent.fdel38-1.fna.fbcdn.net/v/t1.6435-9/121976457_108951920994380_3229862639967779461_n.png?_nc_cat=103&ccb=1-5&_nc_sid=09cbfe&_nc_ohc=OMkvlnnBuJkAX-tvOQE&_nc_ht=scontent.fdel38-1.fna&oh=00_AT8E27K1fFAA80Osbege0Nt9u_6_FU7O8ICdb8s96vvJ8A&oe=621B3C5E",
 "title": "Poets Corner",
 "info": "Poets' Corner group comprises lots of creative and talented poets, rhapsodists and rhymesters, with an urge to stand out in the world of words with their poetry.",
-"link": "https://www.facebook.com/groups/poetscorner01/",
+"link": "https://www.thepoetscorner.org/",
 "rating": "2"
 },
 {
 "id":"5",   
-"img": "",
+"img": "https://scontent.fdel38-1.fna.fbcdn.net/v/t1.6435-9/207353533_10150003289722397_2286655645859758220_n.jpg?_nc_cat=110&ccb=1-5&_nc_sid=8631f5&_nc_ohc=KXG8YmrCT54AX9ryuhY&_nc_ht=scontent.fdel38-1.fna&oh=00_AT_2bFOzkks_GkA71NT3PHNhx3RiPn_KA4O4iJWdxQ5wDg&oe=6218EC9F",
 "title": "Delhi Poetree",
 "info": "Delhi Poetree is a community thats being run in a business-like fashion to promote poetry all across Delhi & NCR.",
 "link": "https://www.facebook.com/groups/delhipoetree/",
@@ -48,7 +48,7 @@ module.exports = [{
 },
 {
 "id":"6",    
-"img": "https://cdn.filestackcontent.com/RHJzJuTZQkSwIyQZO226",
+"img": "https://www.thenextbigwriter.com/images/public/preditors_editors_badge.jpg",
 "title": "TheNextBigWriter",
 "info": "TheNextBigWriter, launched in 2005, combines a dynamic community of thousands of writers with cutting-edge tools, contests, classes, and communication to help members achieve their writing goals.",
 "link": "https://www.thenextbigwriter.com/",
@@ -56,7 +56,7 @@ module.exports = [{
 },
 {
 "id":"7",    
-"img": "",
+"img": "https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.shutterstock.com%2Fsearch%2Fquery&psig=AOvVaw1b1nc-WbeDJadL6ZuZELym&ust=1643452023011000&source=images&cd=vfe&ved=0CAsQjRxqFwoTCPjUo4Ke1PUCFQAAAAAdAAAAABAD",
 "title": "Agent query",
 "info": "AgentQuery is a great place to go for critiques and feedback.",
 "link": "https://agentquery.com/default.aspx",
@@ -64,7 +64,7 @@ module.exports = [{
 },
 {
 "id":"8",
-"img": "",
+"img": "https://scontent.fdel38-1.fna.fbcdn.net/v/t39.30808-6/271206186_2041645019331109_8152334831809383909_n.jpg?_nc_cat=100&ccb=1-5&_nc_sid=09cbfe&_nc_ohc=M3JQJQxpQfEAX8JhwZ0&_nc_ht=scontent.fdel38-1.fna&oh=00_AT-c-tubjN15IIdUIrj_b5-CEzKI7GSjmK965iL-XmZ8cQ&oe=61F83DC9",
 "title": "Underlined",
 "info": "Welcome to our community of Book Nerds and aspiring writers! ",
 "link": "https://www.getunderlined.com/community/",
@@ -72,7 +72,7 @@ module.exports = [{
 },
 {
  "id":"9",   
-"img": "",
+"img": "https://nanowrimo.org/images/logged-out/nanowrimo.svg",
 "title": "NaNoWriMo",
 "info": "NaNoWriMo believes in the transformational power of creativity. ",
 "link": "https://nanowrimo.org/",
