910268d591a78f7a1ee39097e088120fd0bd126b
chirag0002 <varshneychirag34@gmail.com>
2022-01-28 18:47:38 +0530

    Footer Done

--- src/App.js
+++ src/App.js
@@ -6,6 +6,11 @@ import Explore from "./components/Explore";
 import "./styling/App.css";
 import Header from "./components/Header.jsx";
 import Cards from "./cards/Cards";
+import Footer from "./components/Footer.jsx";
+
+
+
+
 const App = () => {
   return (
     <div className="app">
@@ -13,7 +18,9 @@ const App = () => {
         <Route exact path="/ex" element={<Explore />} />
         {/* <Route exact path="/po" element={<Api />} /> */}
         <Route exact path="/header" element={<Header />} />
+        <Route exact path="/footer" element={<Footer />} />
         <Route exact path="/card" element={<Cards />} />
+
       </Routes>
     </div>
   );
